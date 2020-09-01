00bfaf956df352b98a0a49e9e9564e59e57364ec
fuzzworth <nyangasi.m@hotmail.com>
2020-09-02 01:15:18 +0200

    Added Spring Boot Framework

---
+++ src/main/java/com/tutuka/project/Tutuka/TutukaApplication.java
@@ -0,0 +1,13 @@
+package com.tutuka.project.Tutuka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutukaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutukaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutuka/project/Tutuka/TutukaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutuka.project.Tutuka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutukaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
